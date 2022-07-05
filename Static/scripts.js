f456d441965314b6dab1f12ed4f7922ad74769b4
Denis Kirillov <mail@deniskirillov.com>
2022-07-05 04:20:36 +0300

    Add CV

--- Static/scripts.js
+++ Static/scripts.js
@@ -2,7 +2,7 @@ function calculate_age(dob) {
     var diff_ms = Date.now() - dob.getTime();
     var age_dt = new Date(diff_ms);
 
-    return Math.abs(age_dt.getUTCFullYear() - 2000);
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
 
 var element = document.getElementById("ageBlock");
